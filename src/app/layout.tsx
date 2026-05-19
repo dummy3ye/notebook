818579a2ae767ec9ade724df9774309b741cb580
Dummy <146763225+dummy3ye@users.noreply.github.com>
2026-05-19 20:02:58 +0600

    feat : live md preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 import ThemeToggle from "@/components/ThemeToggle";
 import Search from "@/components/Search";
 
