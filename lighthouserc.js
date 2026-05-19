3c53a35f31bd89cba23646fa51229dfed11acd60
Dummy <146763225+dummy3ye@users.noreply.github.com>
2026-05-19 16:21:06 +0600

    chore(ci) : format with prettier

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,24 +1,23 @@
 module.exports = {
-  ci: {
-    collect: {
-      startServerCommand: "npm run start",
-      url: ["http://localhost:3000/"],
-      numberOfRuns: 3,
-      settings: {
-        preset: "desktop",
-      },
+    ci: {
+        collect: {
+            startServerCommand: "npm run start",
+            url: ["http://localhost:3000/"],
+            numberOfRuns: 3,
+            settings: {
+                preset: "desktop",
+            },
+        },
+        upload: {
+            target: "temporary-public-storage",
+        },
+        assert: {
+            assertions: {
+                "categories:performance": ["warn", { minScore: 0.8 }],
+                "categories:accessibility": ["error", { minScore: 0.9 }],
+                "categories:best-practices": ["error", { minScore: 0.9 }],
+                "categories:seo": ["error", { minScore: 0.9 }],
+            },
+        },
     },
-    upload: {
-      target: "temporary-public-storage",
-    },
-    assert: {
-      assertions: {
-        "categories:performance": ["warn", { minScore: 0.8 }],
-        "categories:accessibility": ["error", { minScore: 0.9 }],
-        "categories:best-practices": ["error", { minScore: 0.9 }],
-        "categories:seo": ["error", { minScore: 0.9 }],
-      },
-    },
-  },
 };
-
